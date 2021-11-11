ae246714c362f08da2bd8881599f75cb0d8c5ce2
João PV Correia <correia.jpv@gmail.com>
2021-11-11 22:13:49 +0000

    Favicon hotfix
    Added favicon to webpack bundle

--- webpack.config.js
+++ webpack.config.js
@@ -48,13 +48,15 @@ module.exports = {
       template: './src/views/layout.js',
       page: 'home',
       filename: 'index.html',
-      title: 'Home'
+      title: 'Home',
+      favicon: "./src/img/favicon.ico"
     }),
     new HtmlWebpackPlugin({
       template: './src/views/layout.js',
       page: 'privacy',
       filename: 'privacy.html',
-      title: 'Privacy policy'
+      title: 'Privacy policy',
+      favicon: "./src/img/favicon.ico"
     }),
     new Dotenv({
       path: './.env',
